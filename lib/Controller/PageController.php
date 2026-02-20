047f5deaacff94d356529accb4b454173552e5c3
Gerald Colin <trash@goovy.com>
2026-02-20 22:42:07 +0100

    Enhance PageController with user and group management
    Refactor PageController to include user session and group manager dependencies. Update index method to check if the user is an admin and pass this information to the template response.

--- lib/Controller/PageController.php
+++ lib/Controller/PageController.php
@@ -1,5 +1,4 @@
 <?php
-
 declare(strict_types=1);
 
 namespace OCA\PerfAvion\Controller;
@@ -7,14 +6,17 @@ namespace OCA\PerfAvion\Controller;
 use OCA\PerfAvion\AppInfo\Application;
 use OCP\AppFramework\Controller;
 use OCP\AppFramework\Http\TemplateResponse;
+use OCP\IGroupManager;
 use OCP\IRequest;
+use OCP\IUserSession;
 use OCP\Util;
-
 
 class PageController extends Controller {
 
     public function __construct(
-        IRequest $request
+        IRequest      $request,
+        private IUserSession  $userSession,
+        private IGroupManager $groupManager
     ) {
         parent::__construct(Application::APP_ID, $request);
     }
@@ -25,8 +27,14 @@ class PageController extends Controller {
      */
     public function index(): TemplateResponse {
         Util::addScript(Application::APP_ID, 'perfavion-main');
-        Util::addStyle(Application::APP_ID, 'perfavion-style');
+        Util::addStyle(Application::APP_ID,  'perfavion-style');
 
-        return new TemplateResponse(Application::APP_ID, 'main');
+        $user    = $this->userSession->getUser();
+        $isAdmin = $user !== null
+            && $this->groupManager->isAdmin($user->getUID());
+
+        return new TemplateResponse(Application::APP_ID, 'main', [
+            'isAdmin' => $isAdmin,
+        ]);
     }
 }
